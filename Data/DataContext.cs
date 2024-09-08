710483b8ed818ae4251a6374d220856bf5c274b3
Quade Witt <wittquadeallen@gmail.com>
2024-09-08 18:41:13 -0500

    added datacontext

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,6 +1,20 @@
-﻿namespace groomroom.Data
+﻿using System.Data;
+using System.Reflection;
+using groomroom.Entities;
+using System.Reflection.Emit;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace LearningStarter.Data;
+
+public sealed class DataContext : IdentityDbContext<User, Role, int>
 {
-    public class DataContext
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).GetTypeInfo().Assembly);
     }
 }
